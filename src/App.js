c19b34ea1d1a7e7915c8a3030a34cfa0333246fe
Arif Islam <arifulislam64743@gmail.com>
2023-10-13 01:05:53 +0600

    draggrable to another column

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { DragDropContext } from 'react-beautiful-dnd';
-import Column from './Column/Column';
-import initialData from './InitialData';
-import './App.css';
-import { useState } from 'react';
+import { DragDropContext } from "react-beautiful-dnd";
+import Column from "./Column/Column";
+import initialData from "./InitialData";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  
-  const [data, setData] = useState(initialData)
+  const [data, setData] = useState(initialData);
 
   const { columnOrder, columns, tasks } = data;
 
@@ -25,43 +24,64 @@ function App() {
 
     const start = columns[source.droppableId];
     const finish = columns[destination.droppableId];
-  
+
     if (start === finish) {
       const newTaskIds = Array.from(start.taskIds);
       newTaskIds.splice(source.index, 1);
       newTaskIds.splice(destination.index, 0, draggableId);
-  
+
       const newColumn = {
         ...start,
         taskIds: newTaskIds,
       };
-  
+
       const newColumns = {
         ...columns,
         [newColumn.id]: newColumn,
       };
-  
+
       const newData = {
         ...data,
         columns: newColumns,
       };
-  
+
+      setData(newData);
+    } else {
+      const startTaskIds = Array.from(start.taskIds);
+      startTaskIds.splice(source.index, 1);
+      const newStart = {
+        ...start,
+        taskIds: startTaskIds,
+      };
+
+      const finishTaskIds = Array.from(finish.taskIds);
+      finishTaskIds.splice(destination.index, 0, draggableId);
+      const newFinish = {
+        ...finish,
+        taskIds: finishTaskIds,
+      };
+
+      const newData = {
+        ...data,
+        columns: {
+          ...columns,
+          [newStart.id]: newStart,
+          [newFinish.id]: newFinish,
+        },
+      };
       setData(newData);
     }
-
   }
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
-        {
-          columnOrder.map(columnId => {
-            const column = columns[columnId]
-            const alltasks = column.taskIds.map(taskId => tasks[taskId])
-            // console.log("🚀 ~ file: App.js:21 ~ App ~ alltasks:", alltasks)
-            return <Column key={column.id} column={column} tasks={alltasks} />
-          })
-        }
+        {columnOrder.map((columnId) => {
+          const column = columns[columnId];
+          const alltasks = column.taskIds.map((taskId) => tasks[taskId]);
+          // console.log("🚀 ~ file: App.js:21 ~ App ~ alltasks:", alltasks)
+          return <Column key={column.id} column={column} tasks={alltasks} />;
+        })}
       </div>
     </DragDropContext>
   );
